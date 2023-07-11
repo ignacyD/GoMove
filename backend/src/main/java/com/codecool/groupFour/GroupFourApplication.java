25a55bf199489365477d46fa92f554cb830d5dc3
Jakub <jakub.szczygiel29@gmail.com>
2023-07-11 13:04:45 +0200

    initialize project

---
+++ backend/src/main/java/com/codecool/groupFour/GroupFourApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.groupFour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupFourApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/codecool/groupFour/GroupFourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.groupFour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupFourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
