54c6d48ae383229b30f7d17c4c147d14f77ebfe2
Michał Jeleniewski <m123jeleniewski@gmail.com>
2023-07-27 09:49:47 +0200

    displayMedia Refactor

---
+++ frontend/src/ModalStyles.js
@@ -0,0 +1,13 @@
+const loginFormStyles = {
+    content: {
+        top: '35%',
+        left: '30%',
+        height: "30vh",
+        width: "40vw",
+        display: "flex",
+        flexDirection: "column",
+        border: "4px solid #90ee90",
+        color: "90ee90"
+    }
+};
+export default loginFormStyles;
