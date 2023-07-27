359d79daf12e270563885871ec7f5856be644ae4
Michał Jeleniewski <m123jeleniewski@gmail.com>
2023-07-27 14:22:15 +0200

    LoginForm extended and styled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,21 @@ import LoginForm from "./components/LoginForm/LoginForm";
 import Modal from "react-modal";
 import loginFormStyles from "./ModalStyles";
 import {handleBlur} from "react-modal/lib/helpers/focusManager";
+import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
 
 function App() {
     const [loggedIn, setLoggedIn] = useState(false);
     const [displayLoginForm, setDisplayLoginForm] = useState(false)
+    const [displayRegistrationForm, setDisplayRegistrationForm] = useState(false);
 
     function handleLogout() {
         setDisplayLoginForm(false);
         // wylogowanie użytkownika, wyjście z zakładki profile itp.
+    }
+
+    function closeForms(){
+        setDisplayLoginForm(false);
+        setDisplayRegistrationForm(false);
     }
 
     return (
@@ -21,14 +28,15 @@ function App() {
             <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn} setDisplayLoginForm={setDisplayLoginForm}
                     handleLogout={handleLogout}/>
             <Modal
-                isOpen={displayLoginForm}
-                onRequestClose={() => setDisplayLoginForm(false)}
+                isOpen={displayLoginForm || displayRegistrationForm}
+                onRequestClose={() => closeForms()}
                 contentLabel="Login-modal"
                 style={loginFormStyles}
                 class="login-modal"
                 appElement={document.getElementById("root") || undefined}
             >
-                {displayLoginForm && <LoginForm/>}
+                {displayLoginForm && <LoginForm setDisplayLoginForm={setDisplayLoginForm} setDisplayRegistrationForm={setDisplayRegistrationForm}/>}
+                {displayRegistrationForm && <RegistrationForm setDisplayLoginForm={setDisplayLoginForm} setDisplayRegistrationForm={setDisplayRegistrationForm}/>}
             </Modal>
             <Outlet/>
         </div>
--- frontend/src/ModalStyles.js
+++ frontend/src/ModalStyles.js
@@ -1,9 +1,9 @@
 const loginFormStyles = {
     content: {
         top: '35%',
-        left: '30%',
+        left: '37.5%',
         height: "30vh",
-        width: "40vw",
+        width: "25vw",
         display: "flex",
         flexDirection: "column",
         border: "4px solid #90ee90",
