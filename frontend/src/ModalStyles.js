da21d484c1ef1175a3dd0a413948aa234456c554
Michał Jeleniewski <m123jeleniewski@gmail.com>
2023-07-27 14:32:13 +0200

    LoginForm few more css added

--- frontend/src/ModalStyles.js
+++ frontend/src/ModalStyles.js
@@ -7,7 +7,8 @@ const loginFormStyles = {
         display: "flex",
         flexDirection: "column",
         border: "4px solid #90ee90",
-        color: "90ee90"
+        color: "90ee90",
+        'box-shadow': "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
     }
 };
 export default loginFormStyles;
