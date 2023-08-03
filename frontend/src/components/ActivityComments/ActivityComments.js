9b9d8c16ae57de77c429ed86725f642bc4c85496
KamilSadlocha <sadlocha.kamil@gmail.com>
2023-08-03 13:51:23 +0200

    implement editing comments

--- frontend/src/components/ActivityComments/ActivityComments.js
+++ frontend/src/components/ActivityComments/ActivityComments.js
@@ -5,6 +5,8 @@ import {format} from 'date-fns';
 function ActivityComments({currentActivityID}) {
     const [activityComments, setActivityComments] = useState([]);
     const [newComment, setNewComment] = useState("");
+    const [editingComment, setEditingComment] = useState(null);
+    const [editedMessage, setEditedMessage] = useState("");
 
     const currentDate = new Date();
     const formattedDate = format(currentDate, 'yyyy-MM-dd');
@@ -25,6 +27,12 @@ function ActivityComments({currentActivityID}) {
             console.error('Error fetching activity data:', error);
         }
     };
+
+
+    useEffect(() => {
+        fetchActivityComments();
+    }, []);
+
 
     const handleCommentSubmit = async () => {
         if (newComment.trim() === "") {
@@ -57,10 +65,6 @@ function ActivityComments({currentActivityID}) {
         }
     };
 
-    useEffect(() => {
-        fetchActivityComments();
-    }, []);
-
     const handleDeleteComment = async (comment) => {
         try {
             const response = await fetch(`http://localhost:8080/comments/delete/${comment.commentId}`, {
@@ -79,26 +83,78 @@ function ActivityComments({currentActivityID}) {
         }
     };
 
+    const handleEditComment = (comment) => {
+        setEditingComment(comment);
+        setEditedMessage(comment.message);
+    };
 
-    const handleEditComment = async () => {
-    }
+    const handleCancelEdit = () => {
+        setEditingComment(null);
+        setEditedMessage("");
+    };
 
+    const handleSaveEdit = async () => {
+        try {
+            const updatedComment = {
+                ...editingComment,
+                message: editedMessage
+            };
 
+            const response = await fetch(`http://localhost:8080/comments/update/${editingComment.commentId}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(updatedComment)
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            fetchActivityComments();
+            setEditingComment(null);
+            setEditedMessage("");
+        } catch (error) {
+            console.error('Error editing comment:', error);
+        }
+    };
 
     return (
         <div className="comments">
             <ul>
                 {activityComments.map((comment) => (
                     <li className="commentsList" key={comment.commentId}>
-                        {`${comment.time} ${comment.userId}: ${comment.message}`}
+                        <div>
+                            {editingComment === comment ? (
+                                <div>
+                                    <textarea
+                                        value={editedMessage}
+                                        onChange={(e) => setEditedMessage(e.target.value)}
+                                        onKeyPress={(e) => {
+                                            if (e.key === 'Enter') {
+                                                handleSaveEdit();
+                                            }
+                                        }}
+                                    />
+                                    <button onClick={handleSaveEdit}>Save</button>
 
-
-                        {comment.userId === userID && (
-                            <div>
-                                <button onClick={() => handleEditComment(comment)}>Edit</button>
-                                <button onClick={() => handleDeleteComment(comment)}>Delete</button>
-                            </div>
-                        )}
+                                    <button onClick={handleCancelEdit}>Cancel</button>
+                                </div>
+                            ) : (
+                                <div>
+                                    <span>
+                                        {`${comment.time} ${comment.userId}: ${comment.message}`}
+                                    </span>
+                                    {comment.userId === userID && (
+                                        <div>
+                                            <button onClick={() => handleEditComment(comment)}>Edit</button>
+                                            <button onClick={() => handleDeleteComment(comment)}>Delete</button>
+                                        </div>
+                                    )}
+                                </div>
+                            )}
+                        </div>
                     </li>
                 ))}
             </ul>
