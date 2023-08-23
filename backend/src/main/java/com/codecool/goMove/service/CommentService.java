32f152578f8a2adb11de978a062c98f58326a5d9
dmrozik87 <dmrozik87@gmail.com>
2023-08-23 11:40:54 +0200

    Removed unnecessary code in CommentService constructor

--- backend/src/main/java/com/codecool/goMove/service/CommentService.java
+++ backend/src/main/java/com/codecool/goMove/service/CommentService.java
@@ -1,12 +1,9 @@
 package com.codecool.goMove.service;
 
 import com.codecool.goMove.model.Comment;
-import com.codecool.goMove.model.User;
 import com.codecool.goMove.repository.CommentRepository;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,18 +14,6 @@ public class CommentService {
 
     public CommentService(CommentRepository commentRepository) {
         this.commentRepository = commentRepository;
-        Comment comment1 = new Comment(LocalDate.now(), LocalTime.now(), User.builder().userId(UUID.fromString("2222e1a7-7acf-4f50-8275-1449748e96eb")).build(), "Robimy grila na koniec?", UUID.fromString("1111e4ee-06f5-40ab-935e-442074f939a1"));
-        comment1.setCommentId(UUID.fromString("11110b30-7557-4a9f-8527-3e50e933fec4"));
-        addComment(comment1);
-        Comment comment2 = new Comment(LocalDate.now(), LocalTime.now(), User.builder().userId(UUID.fromString("1111e1a7-7acf-4f50-8275-1449748e96eb")).build(), "No pewnie że tak !", UUID.fromString("1111e4ee-06f5-40ab-935e-442074f939a1"));
-        comment2.setCommentId(UUID.fromString("22220b30-7557-4a9f-8527-3e50e933fec4"));
-        addComment(comment2);
-        Comment comment3 = new Comment(LocalDate.now(), LocalTime.now(), User.builder().userId(UUID.fromString("3333e1a7-7acf-4f50-8275-1449748e96eb")).build(), "To ja wezme ketchup.", UUID.fromString("1111e4ee-06f5-40ab-935e-442074f939a1"));
-        comment3.setCommentId(UUID.fromString("33330b30-7557-4a9f-8527-3e50e933fec4"));
-        addComment(comment3);
-        Comment comment4 = new Comment(LocalDate.now(), LocalTime.now(), User.builder().userId(UUID.fromString("4444e1a7-7acf-4f50-8275-1449748e96eb")).build(), "Na pewno aktualne?", UUID.fromString("2222e4ee-06f5-40ab-935e-442074f939a1"));
-        comment4.setCommentId(UUID.fromString("44440b30-7557-4a9f-8527-3e50e933fec4"));
-        addComment(comment4);
     }
 
     public List<Comment> getAllComments() {
