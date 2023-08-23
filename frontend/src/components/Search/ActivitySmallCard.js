0e537ca6d5f53b6351fcd56dbd369382936c923e
dmrozik87 <dmrozik87@gmail.com>
2023-08-23 17:57:39 +0200

    Clicking ActivitySmallCard in Search navigates to ActivityPage

--- frontend/src/components/Search/ActivitySmallCard.js
+++ frontend/src/components/Search/ActivitySmallCard.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
+import "./ActivitySmallCard.css";
 
 const ActivitySmallCard = ({activity}) => {
+
+    const navigate = useNavigate();
+
     return (
-        <div>
+        <div
+            className="activity-small-card"
+            onClick={() => navigate(`/activity-page/${activity.activityId}`)}
+        >
             <div>{activity.title}</div>
             <div>{activity.activityType}</div>
             <div>{activity.city}, {activity.street}</div>
