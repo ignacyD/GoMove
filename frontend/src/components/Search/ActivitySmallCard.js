c1c1915016fcf52cd3056f1314ef62c17091e3fc
Michał Jeleniewski <m123jeleniewski@gmail.com>
2023-08-24 21:23:02 +0200

    Searched Activities navigate to specific activity

--- frontend/src/components/Search/ActivitySmallCard.js
+++ frontend/src/components/Search/ActivitySmallCard.js
@@ -9,17 +9,22 @@ const ActivitySmallCard = ({activity}) => {
 
     return (
         <div className="searched-activity-card"
-             onClick={() => navigate(`/activity-page/${activity.activityId}`)}>
+        >
             <div className="activity-photo">{activity.photoUrl ?
                 <img src={activity.photoUrl} alt="Activity"/> :
                 <img src={testPhoto} alt="Activity"/>
             }</div>
             <div className="info-section">
-            <div className="activity-title">{activity.title}</div>
-            <div className="activity-type">{activity.activityType}</div>
-            <div className="activity-city">{activity.city}</div>
-            <div className="activity-date">{activity.date}, {activity.time}</div>
-            <button className="join-button">Join</button>
+                <div className="activity-title">{activity.title}</div>
+                <div className="activity-type">{activity.activityType}</div>
+                <div className="activity-city">{activity.city}</div>
+                <div className="activity-date">{activity.date}, {activity.time}</div>
+                <div className="searched-card-buttons">
+                    <button onClick={() => navigate(`/activity-page/${activity.activityId}`)}
+                            className="details-button">Details
+                    </button>
+                <button className="join-button">Join</button>
+                </div>
             </div>
         </div>
     );
