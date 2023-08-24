e9ef690ec685137552772aac820dfd1dd0e772d7
dmrozik87 <dmrozik87@gmail.com>
2023-08-24 09:51:06 +0200

    ActivityPage.js bug fixed

--- frontend/src/components/ActivityPage/ActivityPage.js
+++ frontend/src/components/ActivityPage/ActivityPage.js
@@ -59,20 +59,6 @@ function ActivityPage() {
             }
         })
     }
-    
-    const fetchActivityData = async () => {
-        try {
-            const response = await fetch(`http://localhost:8080/activities/${activityId}`);
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const data = await response.json();
-            setActivityData(data);
-        } catch (error) {
-            console.error('Error fetching activity data:', error);
-        }
-    };
 
     const checkIsUserEnrolled = () => {
         setIsUserEnrolled(activityData.participants.filter(participant =>
